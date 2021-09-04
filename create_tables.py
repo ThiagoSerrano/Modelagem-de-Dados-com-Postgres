edc67a945b51cee4104bdb65592b112399a963a3
ThiagoSerrano <50273450+ThiagoSerrano@users.noreply.github.com>
2021-09-04 12:32:22 -0300

    create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -1,7 +1,6 @@
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
-
 def create_database():
     # connect to default database
     conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
