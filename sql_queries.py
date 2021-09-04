845dd3381cace442c6ed58b6a9fda107ecc6003d
ThiagoSerrano <50273450+ThiagoSerrano@users.noreply.github.com>
2021-09-04 12:34:02 -0300

    sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -94,3 +94,4 @@ WHERE songs.title=%s AND artists.name=%s AND songs.duration=%s
 
 create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+
